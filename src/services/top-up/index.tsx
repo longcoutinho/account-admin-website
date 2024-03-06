cab6546ad3399e90a9c002c26cbca5ed5994889f
han <longcoutinho@gmail.com>
2024-03-06 18:29:30 +0700

    logic

--- src/services/top-up/index.tsx
+++ src/services/top-up/index.tsx
@@ -1,13 +1,10 @@
 import { Backend } from "@/constants";
 import { doGetRequest, doPostRequest, getUserInfo } from "@/constants/FnCommon";
+import {TopUpRequest} from "@/interfaces/request";
 
-export const getAllTopUpRequest = async (): Promise<any> => {
+export const getAllTopUpRequest = async (params: TopUpRequest): Promise<any> => {
   const url = Backend.TOPUP_SERVICE + "/list";
-  const params = {
-      status: 0
-  }
-  return doGetRequest(url, params
-  );
+  return doGetRequest(url, params);
 };
 
 export const confirmTopUpRequest = async (id: string): Promise<any> => {
@@ -16,4 +13,17 @@ export const confirmTopUpRequest = async (id: string): Promise<any> => {
     id: id,
   };
   return doPostRequest(url, request);
+};
+
+export const cancelTopUpRequest = async (id: string): Promise<any> => {
+  const url = Backend.TOPUP_SERVICE + "/cancel";
+  const request = {
+    id: id,
+  };
+  return doPostRequest(url, request);
+};
+
+export const getReportTopUp = async (): Promise<any> => {
+  const url = Backend.REPORT_SERVICE + "/top-up";
+  return doGetRequest(url, null);
 };
