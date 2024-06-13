e58eb252f177f3c8b8ea7b473dbed75cfa78d636
MaiNTP <maintp@onechain.game>
2024-06-13 11:23:03 +0700

    search user, balance

--- src/services/userService/index.tsx
+++ src/services/userService/index.tsx
@@ -1,35 +1,39 @@
-import {Backend, ServiceType} from "@/constants";
-import {doGetRequest, doPostRequest} from "@/constants/FnCommon";
-import axios, {AxiosPromise} from "axios";
+import { Backend, ServiceType } from "@/constants";
+import { doGetRequest, doPostRequest } from "@/constants/FnCommon";
+import axios, { AxiosPromise } from "axios";
 
-export const signUp = async (request: any) : Promise<any> => {
-    const url = Backend.USER_SERVICE + '/register';
-    return doPostRequest(url, request);
-}
+export const signUp = async (request: any): Promise<any> => {
+  const url = Backend.USER_SERVICE + "/register";
+  return doPostRequest(url, request);
+};
 
-export const signIn = async (request: any) : Promise<any> => {
-    const url = Backend.USER_SERVICE + '/login-admin';
-    return doPostRequest(url, request);
-}
+export const signIn = async (request: any): Promise<any> => {
+  const url = Backend.USER_SERVICE + "/login-admin";
+  return doPostRequest(url, request);
+};
 
-export const getUserAccount = async (type: number) => {
-    const url = Backend.USER_SERVICE + '';
-    const request = {
-        type: type
-    };
-    return doGetRequest(url, request);
+export const getUserAccount = async (type: number, userName?: string) => {
+  const url = Backend.USER_SERVICE + "";
+  const request = {
+    type: type,
+    ...(userName !== "" && { username: userName }),
+  };
+  return doGetRequest(url, request);
 };
 
 export const getUserInfoByUsername = async (username: any) => {
-    const url = Backend.USER_SERVICE + '/username/' + username;
-    return doGetRequest(url, null);
+  const url = Backend.USER_SERVICE + "/username/" + username;
+  return doGetRequest(url, null);
 };
 
-export const adjustBalance = async (username: string, amount: number) : Promise<any> => {
-    const params = {
-        username: username,
-        amount: amount
-    }
-    const url = Backend.USER_SERVICE + '/adjust-balance';
-    return doPostRequest(url, params);
-}
+export const adjustBalance = async (
+  username: string,
+  amount: number
+): Promise<any> => {
+  const params = {
+    username: username,
+    amount: amount,
+  };
+  const url = Backend.USER_SERVICE + "/adjust-balance";
+  return doPostRequest(url, params);
+};
